88f35aa6a093e4e4268c6783675b16dcf4f8266b
Kiran <kiran@ideas2it.com>
2020-09-18 23:58:01 -0700

    webdrivermanager

--- ngWedriver/src/test/java/TC1_Login.java
+++ ngWedriver/src/test/java/TC1_Login.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -6,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -13,22 +15,27 @@ import java.util.Arrays;
 
 
 public class TC1_Login {
-    RemoteWebDriver driver;
+    WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
 
     @Test
     public void kiran()  {
         System.out.println("testing driver---------");
-        String path = System.getProperty("user.dir");
-        System.setProperty("webdriver.chrome.driver",path+"/src/test/resources/chromedriver");
-        System.setProperty("webdriver.chrome.whitelistedIps", "");
-        DesiredCapabilities cp1 = DesiredCapabilities.chrome();
-        cp1.setCapability("chrome.switches", Arrays.asList("--disable-popup-blocking"));
-        ChromeOptions options = new ChromeOptions();
-        cp1.setCapability(ChromeOptions.CAPABILITY, options);
-        if (options != null) {
-            cp1.setCapability(ChromeOptions.CAPABILITY, options);
-        }
-        driver = new ChromeDriver(options);
+//        String path = System.getProperty("user.dir");
+//        System.setProperty("webdriver.chrome.driver",path+"/src/test/resources/chromedriver");
+//        System.setProperty("webdriver.chrome.whitelistedIps", "");
+//        DesiredCapabilities cp1 = DesiredCapabilities.chrome();
+//        cp1.setCapability("chrome.switches", Arrays.asList("--disable-popup-blocking"));
+//        ChromeOptions options = new ChromeOptions();
+//        cp1.setCapability(ChromeOptions.CAPABILITY, options);
+//        if (options != null) {
+//            cp1.setCapability(ChromeOptions.CAPABILITY, options);
+//        }
+        driver = new ChromeDriver();
         System.out.println("Login");
         driver.navigate().to("https://t2.devero.com/?site=qaautomation1");
         driver.findElement(By.name("username")).sendKeys("sa");
