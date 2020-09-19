2595866ecabba4313db9d2edab01fcf9dae3573d
Kiran <kiran@ideas2it.com>
2020-09-18 23:07:14 -0700

    n th commit

--- ngWedriver/src/test/java/TC1_Login.java
+++ ngWedriver/src/test/java/TC1_Login.java
@@ -3,24 +3,30 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+
 
 public class TC1_Login {
-
     RemoteWebDriver driver;
+
     @Test
     public void kiran()  {
-
         System.out.println("testing driver---------");
         String path = System.getProperty("user.dir");
         System.setProperty("webdriver.chrome.driver",path+"/src/test/resources/chromedriver");
+        DesiredCapabilities cp1 = DesiredCapabilities.chrome();
+        cp1.setCapability("chrome.switches", Arrays.asList("--disable-popup-blocking"));
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("test-type");
-        options.addArguments("--disable-extensions");
+        cp1.setCapability(ChromeOptions.CAPABILITY, options);
+        if (options != null) {
+            cp1.setCapability(ChromeOptions.CAPABILITY, options);
+        }
         driver = new ChromeDriver(options);
         System.out.println("Login");
         driver.navigate().to("https://t2.devero.com/?site=qaautomation1");
